8dc1a1ce60fc12ec6151672e274389ac11fbb303
nagendrasha191 <nagendrasha191@gmail.com>
2024-02-25 08:21:29 +0000

    new changes

--- src/app/componants/Header.jsx
+++ src/app/componants/Header.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use-client'
+import logo from '../../../assets/logo.png'
 
 const Header = () => {
     return (
@@ -6,8 +7,7 @@ const Header = () => {
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <img src={logo} alt="logo" width={'100px'} height={'50px'} />
                     </a>
                     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
 import Image from "next/image";
 import Header from "./componants/Header";
+import Footer from "./componants/Footer";
+import Slider from "./componants/Slider";
 
 export default function Home() {
   return (
     <>
      <Header/>
+     <Slider/>
+
+     <div className="slate-900">
+      <h3>Hello</h3> 
+
+     </div>
+
+     <Footer/>
     </>
   );
 }
